3de687c56151bc31b0d55005bb18025a6d4151a9
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-16 00:07:53 +0100

    (setup-test-framework):modify the test file to use assert [finishes #159059161]

--- server/models/dummyData.js
+++ server/models/dummyData.js
@@ -1,4 +1,55 @@
-const dummyData = '';
+const dummyData = {
+  users: {
+    1000: {
+      username: 'Abudu Abiodun',
+      gender: 'Male',
+    },
+    1001: {
+      username: 'Solomon Kingsley',
+      gender: 'Male',
+    },
+    1002: {
+      username: 'Abdulsamii Ajala',
+      gender: 'Male',
+    },
+    1003: {
+      username: 'Ngozi Ekekwe',
+      gender: 'Female',
+    },
+  },
+  entries: {
+    entry1: {
+      date: '1st may 2010',
+      userId: 1000,
+      title: 'Last time I ate bread',
+      description: 'The last time I ate bread was Feb 22nd 2009',
+    },
+    entry2: {
+      date: '2nd may 2012',
+      userId: 1001,
+      title: 'Last time I ate bread',
+      description: 'The last time I ate bread was Feb 22nd 2009',
+    },
+    entry3: {
+      date: '3rd may 2013',
+      userId: 1000,
+      title: 'Last time I ate bread',
+      description: 'The last time I ate bread was Feb 22nd 2009',
+    },
+    entry4: {
+      date: '4th may 2014',
+      userId: 1002,
+      title: 'Last time I ate bread',
+      description: 'The last time I ate bread was Feb 22nd 2009',
+    },
+    entry5: {
+      date: '4th may 2014',
+      userId: 1003,
+      title: 'Last time I ate bread',
+      description: 'The last time I ate bread was Feb 22nd 2009',
+    },
+  },
+};
 
 
 export default dummyData;
--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,13 +1,19 @@
 import chai from 'chai';
 import dummyData from '../models/dummyData';
 
-const expect = chai.expect;
+const { assert } = chai;
 
 
 describe('Dummy database structure', () => {
   describe('Dummy Data should be object', () => {
-    it('should be object', () => {
-      expect(dummyData).to.be.a('object');
+    it('should be an object', () => {
+      assert.isObject(dummyData, 'dummyData is an object');
+      assert.property(dummyData, 'users');
+      assert.property(dummyData, 'entries');
+      assert.property(dummyData, 'toString');
+    });
+    it('should have keys: users, entries', () => {
+      assert.hasAllKeys(dummyData, ['users', 'entries']);
     });
   });
 });
