aba6ffc430fbbec1a43422f81441e5996047ad52
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-17 12:28:19 +0100

    (fix-dummy-db): restructure the dummy database [finishes #159103080]

--- server/models/dummyData.js
+++ server/models/dummyData.js
@@ -1,58 +1,59 @@
 const dummyData = {
-  users: {
-    1000: {
+  users: [
+    {
       username: 'Abudu Abiodun',
       gender: 'Male',
       imgUrl: '/aa.jpg',
     },
-    1001: {
+    {
       username: 'Solomon Kingsley',
       gender: 'Male',
       imgUrl: '/sk.jpg',
     },
-    1002: {
+    {
       username: 'Abdulsamii Ajala',
       gender: 'Male',
       imgUrl: '/aa.jpg',
     },
-    1003: {
+    {
       username: 'Ngozi Ekekwe',
       gender: 'Female',
       imgUrl: '/ne.jpg',
     },
-  },
-  entries: {
-    1: {
+  ],
+
+  entries: [
+    {
       date: '1st may 2010',
       userId: 1000,
       title: 'Last time I ate bread',
       description: 'The last time I ate bread was Feb 22nd 2009',
     },
-    2: {
+    {
       date: '2nd may 2012',
       userId: 1001,
       title: 'Last time I ate bread',
       description: 'The last time I ate bread was Feb 22nd 2009',
     },
-    3: {
+    {
       date: '3rd may 2013',
       userId: 1000,
       title: 'Last time I ate bread',
       description: 'The last time I ate bread was Feb 22nd 2009',
     },
-    4: {
+    {
       date: '4th may 2014',
       userId: 1002,
       title: 'Last time I ate bread',
       description: 'The last time I ate bread was Feb 22nd 2009',
     },
-    5: {
+    {
       date: '4th may 2014',
       userId: 1003,
       title: 'Last time I ate bread',
       description: 'The last time I ate bread was Feb 22nd 2009',
     },
-  },
+  ],
 };
 
 export default dummyData;
