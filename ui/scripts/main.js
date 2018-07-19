527301475a8d804297c71966ba153acf12c29e4d
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-19 11:49:05 +0100

    (ch-deploy-to-heroke): add procfile [#159163569]

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -3,22 +3,22 @@ const modal = document.getElementById('modalBox');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
 
 
 const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "0";
-}
+  document.getElementById('mySidenav').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '0';
+};
 
 const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
+  document.getElementById('mySidenav').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+};
 
-const redirect= () => {
-    window.location="./user-entries.html";
- }
+const redirect = () => {
+  window.location = './user-entries.html';
+};
