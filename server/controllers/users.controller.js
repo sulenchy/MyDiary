1f076360eb52670027028257dbd5263639973106
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-26 18:28:41 +0100

    fix(secure-post-auth-signup):secure post /auth/signup with jwt[fixes #159305579]

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import connection from '../helpers/connection';
+import createToken from '../helpers/createToken';
 
 
 const client = connection();
@@ -12,16 +13,16 @@ client.connect();
  *
  */
 export default class UsersController {
-  /**
-   * @description - Creates a new user
-   * @static
-   *
-   * @param {object} request - HTTP Request
-   * @param {object} response- HTTP Response
-   *
-   * @memberof userController
-   *
-   */
+/**
+ * @description - Creates a new user
+ * @static
+ *
+ * @param {object} request - HTTP Request
+ * @param {object} response- HTTP Response
+ *
+ * @memberof userController
+ *
+ */
   static signupUser(req, res) {
     const password = bcrypt.hashSync(req.body.password.trim(), 10);
     const {
@@ -32,22 +33,24 @@ export default class UsersController {
     INSERT INTO users (fullname,email,password,gender)
     VALUES ('${fullname}','${email}','${password}','${gender}') RETURNING *;`;
     client.query(user)
-      .then(newUser => res.status(201)
-        .json({
-          data: {
-            user: {
-              id: newUser.rows[0].id,
-              fullname,
-              email,
-              gender,
-              notification: newUser.rows[0].notification,
-              role: newUser.rows[0].role,
+      .then((newUser) => {
+        const token = createToken(newUser.rows[0]);
+        return res.status(201)
+          .json({
+            data: {
+              user: {
+                id: newUser.rows[0].id,
+                fullname,
+                email,
+                gender,
+                notification: newUser.rows[0].notification,
+                role: newUser.rows[0].role,
+              },
+              token,
             },
-          },
-          message: 'user created successfully',
-          status: 'success',
-        })).catch((err) => {
-        res.status(500).send(err);
-      });
+            message: 'user created successfully',
+            status: 'success',
+          });
+      }).catch((err) => { res.status(500).send(err); });
   }
 }
