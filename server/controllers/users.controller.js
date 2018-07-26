632d610e84a44fad77286bb47d2cfca509302293
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-26 18:42:16 +0100

    fix(secure-post-auth-signup):reduce the line from 27 to 25 [fixes #159305579]

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -2,7 +2,6 @@ import bcrypt from 'bcrypt';
 import connection from '../helpers/connection';
 import createToken from '../helpers/createToken';
 
-
 const client = connection();
 client.connect();
 
@@ -16,7 +15,7 @@ export default class UsersController {
 /**
  * @description - Creates a new user
  * @static
- *
+ *  *
  * @param {object} request - HTTP Request
  * @param {object} response- HTTP Response
  *
@@ -28,29 +27,27 @@ export default class UsersController {
     const {
       fullname, email, gender,
     } = req.body;
-
     const user = `
     INSERT INTO users (fullname,email,password,gender)
     VALUES ('${fullname}','${email}','${password}','${gender}') RETURNING *;`;
     client.query(user)
       .then((newUser) => {
         const token = createToken(newUser.rows[0]);
-        return res.status(201)
-          .json({
-            data: {
-              user: {
-                id: newUser.rows[0].id,
-                fullname,
-                email,
-                gender,
-                notification: newUser.rows[0].notification,
-                role: newUser.rows[0].role,
-              },
-              token,
+        return res.status(201).json({
+          data: {
+            user: {
+              id: newUser.rows[0].id,
+              fullname,
+              email,
+              gender,
+              notification: newUser.rows[0].notification,
+              role: newUser.rows[0].role,
             },
-            message: 'user created successfully',
-            status: 'success',
-          });
+            token,
+          },
+          message: 'user created successfully',
+          status: 'success',
+        });
       }).catch((err) => { res.status(500).send(err); });
   }
 }
