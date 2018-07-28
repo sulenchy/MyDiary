babb1afdec79557251251e3234247024c9b03746
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-28 16:59:49 +0100

    feat(POST-/auth/login)Initialize the auth login
    -testing the previous tests
    
    [Delivers #159304475]

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -40,10 +40,6 @@ describe('Test default route', () => {
   });
 });
 describe('POST /api/v1/auth/signup', () => {
-  beforeEach((done) => {
-    resetDb();
-    done();
-  });
   it('It Should create users with right signup details', (done) => {
     chai.request(app)
       .post(`${signupUrl}`)
