3bf3a644ea4fd554f1a0a3c9e8ae355db429a1ce
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-07-29 23:58:58 +0100

    feat(POST-/entries api endpoint): POST /entries data persistence
    -add the endpoint
    -add its tests
    
    [deliver #159305199]

--- server/helpers/entries.helper.js
+++ server/helpers/entries.helper.js
@@ -1,61 +1,21 @@
-import dummyData from '../models/dummyData';
+
+import connection from './connection';
+
+const client = connection();
+client.connect();
 
 
 export default class EntriesHelper {
-  static getAllEntries() {
+  
+  static createEntry(userid, title, content) {
+    const entry = `INSERT INTO entries (userid, title, content) VALUES (${userid},'${title}','${content}') RETURNING *;`;
     return new Promise((resolve, reject) => {
-      const data = dummyData.entries;
+      const data = client.query(entry);
       if (data) {
         resolve(data);
       } else {
         reject(new Error({
-          message: 'Request unsuccessful',
-        }));
-      }
-    });
-  }
-
-  static getDiaryEntryById(entryId) {
-    return new Promise((resolve, reject) => {
-      const data = dummyData.entries[entryId];
-      if (data) {
-        resolve(data);
-      } else {
-        reject(new Error('Diary entry cannot be found'));
-      }
-    });
-  }
-
-  static addNewDiaryEntry(date, time, userId, title, description) {
-    return new Promise((resolve, reject) => {
-      dummyData.entries.push({
-        date,
-        time,
-        userId,
-        title,
-        description,
-      });
-      const data = dummyData.entries[dummyData.entries.length - 1];
-      if (data) {
-        resolve(data);
-      } else {
-        reject(new Error({
-          message: 'Sorry, New diary entry cannot be created',
-        }));
-      }
-    });
-  }
-
-  static updateDiaryEntry(id, update) {
-    return new Promise((resolve, reject) => {
-      const existingEntry = dummyData.entries[id];
-      const updatedData = Object.assign(existingEntry, update);
-      if (updatedData) {
-        dummyData.entries[id] = updatedData;
-        resolve(updatedData);
-      } else {
-        reject(new Error({
-          message: 'Sorry, diary entry cannot be updated',
+          message: 'Sorry, New entry cannot be created',
         }));
       }
     });
---
+++ server/middlewares/authenticateLogin.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+const secretKey = process.env.JWT_SECRET;
+
+/**
+ * @class Authenticate User
+ */
+export default class AuthenticateUserLogin {
+  /**
+     * Authenticate Get All Requests
+     *
+     * @param {Object} request
+     * @param {Object} response
+     *
+     * @param {Function} next
+     *
+     * @return {Object}
+     */
+  static authenticateUser(req, res, next) {
+    try {
+      const token = req.headers['x-access'] || req.headers.token || req.query.token;
+      const verifiedToken = jwt.verify(token, secretKey);
+      req.token = verifiedToken;
+      return next();
+    } catch (error) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'User is unauthorized',
+      });
+    }
+  }
+}
