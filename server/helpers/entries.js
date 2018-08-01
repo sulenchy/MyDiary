3d7a6db3ff3a72da464fe460c0a5f91acd5186ad
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-01 16:11:47 +0100

    feat(get-entry-API): feedback implementation
    -modifies the documentation in entries helper and entries contoller files
    
    [delivers #159304682]

--- server/helpers/entries.js
+++ server/helpers/entries.js
@@ -4,15 +4,16 @@ const client = connection();
 client.connect();
 
 export default class EntriesHelper {
-  /* @description - Creates a new Entry
- * @static
- *  *
- * @param {object} request - HTTP Request
- * @param {object} response- HTTP Response
- *
- * @memberof EntriesHelper
- *
- */
+  /**
+   * @description create new entry
+   *
+   * @param {*} userid
+   * @param {*} title
+   * @param {*} content
+   *
+   * @memberOf EntriesHelper Class
+   */
+
   static createEntry(userid, title, content) {
     const entry = `INSERT INTO entries (userid, title, content) VALUES (${userid},'${title}','${content}') RETURNING *;`;
     return new Promise((resolve, reject) => {
@@ -27,24 +28,44 @@ export default class EntriesHelper {
     });
   }
 
-  /* @description - gets all entries
- * @static
- *  *
- * @param {object} request - HTTP Request
- * @param {object} response- HTTP Response
+  /**
+ * @description get all entries for a particular user
  *
- * @memberof EntriesHelper
+ * @param {*} userid
  *
+ * @memberOf EntriesHelper Class
  */
   static getAllEntry(userid) {
-    const entry = `SELECT id, title, content FROM entries WHERE userid= ${userid};`;
+    const entries = `SELECT id, title, content FROM entries WHERE userid= ${userid};`;
+    return new Promise((resolve, reject) => {
+      const data = client.query(entries);
+      if (data) {
+        resolve(data);
+      } else {
+        reject(new Error({
+          message: 'Sorry, No entry is currently available for the user',
+        }));
+      }
+    });
+  }
+
+  /**
+   * @description get an entry for a user
+   *
+   * @param {*} userid
+   * @param {*} id
+   *
+   * @memberOf EntriesHelper Class
+   */
+  static getEntry(userid, id) {
+    const entry = `SELECT id, title, content FROM entries WHERE USERID=${userid} AND id=${id}`;
     return new Promise((resolve, reject) => {
       const data = client.query(entry);
       if (data) {
         resolve(data);
       } else {
         reject(new Error({
-          message: 'Sorry, No entry is currently available for the user',
+          message: 'Entry not found',
         }));
       }
     });
