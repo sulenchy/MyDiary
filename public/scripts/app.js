f55b5c2065b854037c7ca476de01723d5c5939e9
Sulenchy <Sulaimanabiodun172@gmail.com>
2018-08-13 23:16:43 +0100

    feat(add-entry-client-side): consume add entry api endpoint
    - modify the landing-page.html
    - modify the app.js
    - modify main.css
    - modify the add-edit.html
    
    [finishes #159719935]

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,6 @@
+let token = '';
+const entryUrl = 'http://localhost:8080/api/v1/entries';
+
 // loads all entries of a user
 const viewAllEntries = () => {
   return `<div> <h1>All entries for an authorised user</h1></div>`;
@@ -8,8 +11,44 @@ const viewEntry = () => {
   return `<div> <h1>An entry of an authorised user</h1></div>`;
 };
 
-const addNewEntry = () => {
-  return `<div> <h1>Adds new diary entry</h1></div>`;
+const addNewEntry = (event) => {
+  event.preventDefault();
+  document.getElementById('add_new_error').innerHTML = '';
+
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  token = localStorage.getItem('token');
+
+  fetch(entryUrl, {
+    method: 'POST',
+    mode: 'cors',
+    headers: { 
+      'Content-Type': 'application/json',
+      Token: token,
+     },
+    body: JSON.stringify({
+      title, content,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 409) {
+        document.getElementById('add_new_error').innerHTML = 'Email already exists';
+      }
+      return response.json();
+    })
+    .then((user) => {
+      if (user.data.errors) {
+        console.log(user.data.errors);
+        Object.keys(user.data.errors).forEach((key) => {
+          const ul = document.getElementById('add_new_error');
+          const li = document.createElement('li');
+          li.appendChild(document.createTextNode(user.data.errors[key]));
+          ul.appendChild(li);
+        });
+      } else {
+        alert(`An entry has been added successfully`);
+      }
+    }).catch(err => err.message);
 };
 
 const editEntry = () => {
@@ -20,6 +59,9 @@ const deleteEntry = () => {
   return `<div> <h1>Delete an entry</h1></div>`;
 };
 
+const showAddNewForm = () => {
+  document.getElementById('modalBox').style.display = 'block';
+};
 
 const userProfile = () => {
   return `<div> <h1>Shows the profile of the user</h1></div>`;
@@ -31,3 +73,7 @@ const logout = () => {
 }
 
 document.getElementById('logout').addEventListener('click', logout);
+document.getElementById('add_new').addEventListener('click', showAddNewForm);
+
+
+document.getElementById('file-submit').addEventListener('click', addNewEntry);
